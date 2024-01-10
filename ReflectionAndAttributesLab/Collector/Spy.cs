f4c287c2afddcf1de1d9bd4b7f3619c6cd4d0b22
Martoka <hitmanspybg@gmail.com>
2024-01-10 12:58:24 +0200

    +

---
+++ ReflectionAndAttributesLab/Collector/Spy.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+
+namespace Stealer
+{
+	public class Spy
+	{
+		public string CollectGettersAndSetters(string investigatedClass)
+		{
+			Type classType = Type.GetType(investigatedClass);
+			MethodInfo[] classMethods = classType.GetMethods(BindingFlags.Instance 
+				| BindingFlags.Public
+				| BindingFlags.NonPublic);
+			StringBuilder sb = new StringBuilder();
+
+			foreach (MethodInfo method in classMethods.Where(m => m.Name.StartsWith("get")))
+			{
+				sb.AppendLine($"{method.Name} will return {method.ReturnType}");
+			}
+			foreach (MethodInfo method in classMethods.Where(m => m.Name.StartsWith("set")))
+			{
+				sb.AppendLine($"{method.Name} will set field of {method.GetParameters().First().ParameterType}");
+			}
+			return sb.ToString().Trim();
+		}
+	}
+}
---
+++ WormAndHoles/TheGamblerProblem/Program.cs
@@ -0,0 +1,100 @@
+﻿int n = int.Parse(Console.ReadLine());
+char[,] board = new char[n, n];
+
+int playerRow = -1;
+int playerCol = -1;
+int initialAmount = 100;
+
+for (int i = 0; i < n; i++)
+{
+	string row = Console.ReadLine();
+	for (int j = 0; j < n; j++)
+	{
+		board[i, j] = row[j];
+		if (row[j] == 'G')
+		{
+			playerRow = i;
+			playerCol = j;
+		}
+	}
+}
+
+string command = Console.ReadLine();
+while (command != "end")
+{
+	int newRow = playerRow;
+	int newCol = playerCol;
+
+	switch (command)
+	{
+		case "up":
+			newRow--;
+			break;
+		case "down":
+			newRow++;
+			break;
+		case "left":
+			newCol--;
+			break;
+		case "right":
+			newCol++;
+			break;
+	}
+
+	if (newRow < 0 || newRow >= n || newCol < 0 || newCol >= n)
+	{
+		Console.WriteLine("Game over! You lost everything!");
+		return;
+	}
+
+	char newPosition = board[newRow, newCol];
+
+	if (newPosition == '-')
+	{
+		board[playerRow, playerCol] = '-';
+		board[newRow, newCol] = 'G';
+		playerRow = newRow;
+		playerCol = newCol;
+	}
+	else if (newPosition == 'W')
+	{
+		initialAmount += 100;
+		board[playerRow, playerCol] = '-';
+		board[newRow, newCol] = 'G';
+		playerRow = newRow;
+		playerCol = newCol;
+	}
+	else if (newPosition == 'P')
+	{
+		initialAmount -= 200;
+		if (initialAmount <= 0)
+		{
+			Console.WriteLine("Game over! You lost everything!");
+			return;
+		}
+		board[playerRow, playerCol] = '-';
+		board[newRow, newCol] = 'G';
+		playerRow = newRow;
+		playerCol = newCol;
+	}
+	else if (newPosition == 'J')
+	{
+		initialAmount += 100000;
+		Console.WriteLine("You win the Jackpot!");
+		break;
+	}
+
+	command = Console.ReadLine();
+}
+
+Console.WriteLine("End of the game. Total amount: " + initialAmount + "$");
+board[playerRow, playerCol] = 'G';
+
+for (int i = 0; i < n; i++)
+{
+	for (int j = 0; j < n; j++)
+	{
+		Console.Write(board[i, j]);
+	}
+	Console.WriteLine();
+}
